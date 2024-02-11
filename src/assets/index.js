24df6eaf346efb254e8da283b712a71d20f6fe17
George <georgerubaylo@gmail.com>
2024-02-11 14:17:15 +0300

    Сохранение локальных изменений перед слиянием

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,8 @@ import cnii_logo from './cnii.svg'
 import login_logo from './account.svg'
 import settings_logo from './settings.svg'
 import searchAside_logo from './searchAside.svg'
+import russia_logo from './russia.svg'
 
 export {
-    cnii_logo, login_logo, settings_logo, searchAside_logo
+    cnii_logo, login_logo, settings_logo, searchAside_logo, russia_logo
 }         
